f1dc022778b09cb604448b84421e4d1474850c40
Tedros <71509973+Tedrosgh@users.noreply.github.com>
2022-01-05 15:55:10 +0100

    drag and drop

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, useNavigate, Route, Routes } from 'react-router-dom';
+import PrimarySearchAppBar from './components/navigation.js';
+import Sundaysperformance from './pages/sunday.js';
+import Einnahme from './pages/Einnahme.js';
+import Ausgabe from './pages/Ausgabe.js';
+import Events from './pages/Events.js';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>EEGK</h1>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <PrimarySearchAppBar />
+        <h1>EEGK</h1>
+        <Routes>
+          <Route exact path='/' element={<Sundaysperformance />}></Route>
+          <Route exact path='/events' element={<Events />}></Route>
+          <Route exact path='/einnahme' element={<Einnahme />}></Route>
+          <Route exact path='/ausgabe' element={<Ausgabe />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
